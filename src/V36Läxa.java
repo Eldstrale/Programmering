06372dba2c592e185c15d7e9f9f2456841a8f444
LilBartholomew <calle.eld@hotmail.com>
2018-09-07 15:08:05 +0200

    namn

--- src/V36Läxa.java
+++ src/V36Läxa.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class V36Läxa {
 	public static void main(String[] args) {
 	
 		Scanner input = new Scanner(System.in);
 		
-	}
+		System.out.println("Vad heter du?");
+		
+		String namn = input.nextLine("Carl");
+		
+		
+		}
 
 }
