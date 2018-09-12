3f4dfb76983c363d37f16931438824c847aec148
LilBartholomew <calle.eld@hotmail.com>
2018-09-12 10:38:13 +0200

    Nytt dokument
    Råkade göra någonting med filen så jag var tvungen att kopiera över till en ny fil, hoppas att du kan se alla mina tidigare commits.

---
+++ src/Laxa.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Laxa {
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Vad heter du?");
+		
+		String namn = input.nextLine();
+		
+		System.out.println(namn);
+		
+		System.out.println("Hur gammal är du?");
+		
+		String age = input.nextLine();
+		
+		System.out.println(age);
+		
+		System.out.println("Vad är din adress?");
+		
+		String adress = input.nextLine();
+		
+		System.out.println(adress);
+		
+		System.out.println("Vad är ditt postnummer?");
+		
+		String postnummer = input.nextLine();
+				
+		System.out.println(postnummer);
+		
+		System.out.println("Vilken stad bor du i?");
+		
+		String stad = input.nextLine();
+		
+		System.out.println(stad);
+
+		System.out.println("Vad är ditt telefonnummer?");
+		
+		String telefonnummer = input.nextLine();
+		
+		System.out.println(telefonnummer);
+		
+		}
+
+}
