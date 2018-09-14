4c196a04bfefdd29bdfb2d37d1d018f51ca55d54
LilBartholomew <calle.eld@hotmail.com>
2018-09-14 14:50:00 +0200

    l

--- src/s.java
+++ src/s.java
@@ -30,7 +30,7 @@ public class s {
 }
 
 
-if(d>tal1)else if(d>tal2)if tal 1>
-int summa = tal1+tal2+tal3+tal4+tal5+tal6+tal7+tal8+tal9+tal10
-double medel1 = summa / 5; // 5
-double medel2 = summa / 5.0; // 5,6
+// if(d>tal1)else if(d>tal2)if tal 1>
+// int summa = tal1+tal2+tal3+tal4+tal5+tal6+tal7+tal8+tal9+tal10
+// double medel1 = summa / 5; // 5
+// double medel2 = summa / 5.0; // 5,6
