0e98cc72fa69ce0d0bbcb6568dfa01fab7e768a9
LilBartholomew <calle.eld@hotmail.com>
2018-09-14 14:49:27 +0200

    start läxa 2

--- src/s.java
+++ src/s.java
@@ -1,4 +1,36 @@
+import java.util.Scanner;
 
 public class s {
 
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int max = 99999999;
+		int min = -99999999;
+		
+		
+		int tal1 = input.nextInt();
+		int tal2 = input.nextInt();
+		int tal3 = input.nextInt();
+		int tal4 = input.nextInt();
+		int tal5 = input.nextInt();
+		int tal6 = input.nextInt();
+		int tal7 = input.nextInt();
+		int tal8 = input.nextInt();
+		int tal9 = input.nextInt();
+		int tal10 = input.nextInt();
+		
+		
+		
+		
+		
+	}
+
 }
+
+
+if(d>tal1)else if(d>tal2)if tal 1>
+int summa = tal1+tal2+tal3+tal4+tal5+tal6+tal7+tal8+tal9+tal10
+double medel1 = summa / 5; // 5
+double medel2 = summa / 5.0; // 5,6
