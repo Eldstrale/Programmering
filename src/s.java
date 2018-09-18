fac0e829bcdca663e61a5715282b69fbddf21c0c
LilBartholomew <calle.eld@hotmail.com>
2018-09-18 16:36:58 +0200

    Klar

--- src/s.java
+++ src/s.java
@@ -6,7 +6,7 @@ public class s {
 
 		Scanner input = new Scanner(System.in);
 
-		
+		System.out.println("SKRIV 10 TAL");
 		
 		int max = 99999999;
 		int min = -99999999;
@@ -48,15 +48,73 @@ public class s {
 		
 		double medel = summa / 10; // 5
 		
-		System.out.println("Minsta: " + min);
-		System.out.println("Största: " + max);
+		int stor = tal1;
+		
+		if (stor <= tal2) {
+			stor = tal2;
+		}
+		if (stor <= tal3) {
+			stor = tal3;
+		}
+		if (stor <= tal4) {
+			stor = tal4;
+		}
+		if (stor <= tal5) {
+			stor = tal5;
+		}
+		if (stor <= tal6) {
+			stor = tal6;
+		}
+		if (stor <= tal7) {
+			stor = tal7;
+		}
+		if (stor <= tal8) {
+			stor = tal8;
+		}
+		if (stor <= tal9) {
+			stor = tal9;
+		}
+		if (stor <= tal10) {
+			stor = tal10;
+		}
+		
+		
+		int minst = tal1;
+		
+		if (minst >= tal2) {
+			minst = tal2;
+		}
+		if (minst >= tal3) {
+			minst = tal3;
+		}
+		if (minst >= tal4) {
+			minst = tal4;
+		}
+		if (minst >= tal5) {
+			minst = tal5;
+		}
+		if (minst >= tal6) {
+			minst = tal6;
+		}
+		if (minst >= tal7) {
+			minst = tal7;
+		}
+		if (minst >= tal8) {
+			minst = tal8;
+		}
+		if (minst >= tal9) {
+			minst = tal9;
+		}
+		if (minst >= tal10) {
+			minst = tal10;
+		}
+		
+		
+		
+		System.out.println("Minsta: " + minst);
+		System.out.println("Största: " + stor);
 		System.out.println("Medel: " + medel);
 		
 	}
 
 }
-
-// if(d>tal1)else if(d>tal2)if tal 1>
-// int summa = tal1+tal2+tal3+tal4+tal5+tal6+tal7+tal8+tal9+tal10
-// double medel1 = summa / 5; // 5
-// double medel2 = summa / 5.0; // 5,6
