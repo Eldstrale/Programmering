3b8484802547c451aa332400f470c24fd8f76179
LilBartholomew <calle.eld@hotmail.com>
2018-09-21 14:23:08 +0200

    .

---
+++ src/TimeLoop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TimeLoop {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner System.in;
+		
+		int min 1;
+		
+		int max 100;
+		
+		int tal input.nextInt();
+		
+	}
+	
+}
