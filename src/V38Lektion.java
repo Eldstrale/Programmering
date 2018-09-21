3c9d6dcfae714c7bc8ab91bbb859db4d06a3cdf3
LilBartholomew <calle.eld@hotmail.com>
2018-09-21 14:15:27 +0200

    quadrant klar

---
+++ src/V38Lektion.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class V38Lektion {
+	
+		public static void main(String[] args) {
+		
+			Scanner input = new Scanner(System.in);
+
+			
+			
+			for (int i = 20; i <= 0; i =-1) {
+				
+				
+			
+		
+			System.out.println(i);
+		}
+			
+	}
+		
+}
+
+// Skriver ut 0-4
+//		for (int i = 0; i < 5; i++) {
+//		System.out.println(i);			
+//	}
+
+// Skriver ut 5-1
+//	for (int i = 5 ; i > 0; i--) {
+//			System.out.println(i);
+//	}
+
+// Skriver ut alla jämna tal under 100
+//for (int i = 0; i < 100; i += 2) {
+	//System.out.println(i);
+//	}
