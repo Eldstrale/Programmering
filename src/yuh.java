54643da9190714e70710d587f4e80f3612172517
LilBartholomew <calle.eld@hotmail.com>
2018-09-28 14:39:01 +0200

    12

--- src/yuh.java
+++ src/yuh.java
@@ -5,12 +5,16 @@ public class yuh {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-		
+
+		System.out.println("Skriv två tal");
+
 		int ett = input.nextInt();
 		int tva = input.nextInt();
-		if (ett>tva)System.out.println(tva);
-		else System.out.println(ett);
-		
+		if (ett > tva)
+			System.out.println(tva + " Är minst");
+		else
+			System.out.println(ett + " Är minst");
+
 	}
-	
+
 }
