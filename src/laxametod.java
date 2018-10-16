170e4e9dd224ff8069e8d93a243e56a9f13db839
LilBartholomew <calle.eld@hotmail.com>
2018-10-16 14:17:52 +0200

    2Klar

--- src/laxametod.java
+++ src/laxametod.java
@@ -1,13 +1,21 @@
-import jav.util.Scanner;
+import java.util.Scanner;
 
 public class laxametod {
 
 	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);	
+	System.out.println(reverse(input.nextLine()));
 	}
 	public static String reverse(String str) {
+		
+		int length =str.length();
+		char[] arr = new char[length];
+		for(int i = length -1; i >= 0; i--) {
+			arr[length-(i+1)] = str.charAt(i);
 		}
 	
+		String s = new String(arr);
+		return s;
 	}
 
 }
