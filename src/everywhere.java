34430f3c20876e12b4eb7c3b7c1332081cb2d888
LilBartholomew <calle.eld@hotmail.com>
2018-12-14 13:50:27 +0100

    .

---
+++ src/everywhere.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Scanner;
+public class everywhere {
+	public static void main(String[] args) {
+		Scanner sn = new Scanner(System.in);
+		Short t = sn.nextShort();
+		Short n=0;
+		HashSet<String> cities = new HashSet<String>();
+		for(int i = 0; i< t; i++) {
+			n = sn.nextShort();
+			for(int j =0; j<n; j++) {
+				cities.add(sn.next());
+			}
+			System.out.println(cities.size());
+			cities.clear();
+		}
+	}
+}
---
+++ src/lektionv46_2.java
@@ -0,0 +1,3 @@
+public class lektionv46_2 {
+
+}
